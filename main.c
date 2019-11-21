b87a7b1404c7420eb7e81850b86074d3f895b89a
seyoungyook <xqd1219@naver.com>
2019-11-21 15:22:39 +0900

    practice5

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	char input[100];
+	FILE *fp;
+	
+	fp = fopen("sample.txt","w");
+	for(i=0;i<3;i++)
+		{
+		printf("input a word : ");
+		scanf("%s", input);
+		fprintf(fp,"%s\n", input);
+		}	
+	
+	fclose(fp);
+	return 0;
+}
